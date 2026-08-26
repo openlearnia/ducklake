dfc484100f90363e62fc518bf4bec79eb69756d3
Kartik Bazzad <kartikbazzad@gmail.com>
2026-08-26 21:55:31 +0530

    Remove WASM playground build

--- wasm-playground/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import viteConfig from './vite.config';
-
-export default defineConfig({
-  ...viteConfig,
-  test: {
-    exclude: ['node_modules', 'dist', 'tests/**'],
-  },
-});
