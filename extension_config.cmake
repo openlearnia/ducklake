1b292cb5a7f567a5359c2ea6cb0c726e78d19f4d
Kartik Bazzad <kartikbazzad@gmail.com>
2026-09-16 12:50:16 +0530

    ci: quack pin to 200c76c (drop local SetFallible, core patch owns it)

--- extension_config.cmake
+++ extension_config.cmake
@@ -49,7 +49,7 @@ if($ENV{ENABLE_QUACK})
             # openlearnia fork: b2f2d10 ported to the cyanoptera v2.0.0-alpha
             # core APIs (Identifier/QualifiedName), RPC behavior unchanged.
             GIT_URL https://github.com/openlearnia/duckdb-quack.git
-            GIT_TAG b05b7a16193295669c221717a30c86aecc178aa9
+            GIT_TAG 200c76caed49b8b983c7ae885a5031e26754eb6f
             SUBMODULES "extension-ci-tools"
             APPLY_PATCHES
     )
